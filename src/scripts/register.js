de47188a38bd8946d6616784e9d5fe903584217b
Lucas Guimarães <129208350+lucasguimaraes2005@users.noreply.github.com>
2023-09-11 20:01:19 -0300

    Update register.js

--- src/scripts/register.js
+++ src/scripts/register.js
@@ -1,1 +1,28 @@
-// Desenvolva as funcionalidades de cadastro aqui
+const handleRegister = () => {
+  const inputs = document.querySelectorAll(".register__input");
+  const button = document.querySelector("#register__submit");
+
+  button.addEventListener("click", async (event) => {
+    event.preventDefault();
+
+    let count = 0;
+
+    const requestBody = {};
+
+    inputs.forEach((input) => {
+      if (input.value.trim() === "") {
+        count++;
+      } else {
+        requestBody[input.name] = input.value.trim();
+      }
+    });
+
+    if (count === 0) {
+      const response = await registerRequest(requestBody);
+      // Aqui você pode adicionar qualquer lógica adicional após um registro bem-sucedido
+    } else {
+      toast("Por favor, preencha todos os campos!", red);
+    }
+  });
+};
+
